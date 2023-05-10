ab2277b617cdd37f314dc91626e5294f5d3875cc
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-05-10 14:24:30 -0500

    creacion del proyecto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Tienda de ropa
+        </p>
+        <a>
+          Bienvenido a la tienda
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
