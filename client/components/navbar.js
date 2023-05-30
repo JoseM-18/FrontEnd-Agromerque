ae3ebde5f8b1d5270df820bc7f5f6e51ea42d843
JoseM-18 <91767929+JoseM-18@users.noreply.github.com>
2023-05-29 22:12:05 -0500

    ajustes iniciales del front

---
+++ client/components/navbar.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <div> 
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        a
+        </nav>
+
+    </div>
+  )
+}
+
--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Tienda de ropa
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a>
-          Bienvenido a la tienda
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
       </header>
     </div>
