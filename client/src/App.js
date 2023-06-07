e7183a4e11deacb7acdf1519bf840d423f39f547
a-garcia07 <andres.m.munoz@correounivalle.edu.co>
2023-06-07 12:22:00 -0500

    Mostrar data local de productos

--- client/src/App.js
+++ client/src/App.js
@@ -6,14 +6,19 @@ import Target from './components/Target'
 import ShoppingCart from './components/ShoppingCart'
 import { Container } from '@mui/material'
 
+import productosDta from './data/products.json'
+
 function App() {
+
+  const productos = productosDta.productos;
+
   return (
 
     <BrowserRouter>
       <Navbar />
       <Container>
         <Routes>
-          <Route path="/" element={ <Target /> } />
+          <Route path="/" element={ <Target products={ productos }/> } />
           <Route path="/login" element={ <LoginForm /> } />
           <Route path= "/register" element ={<RegisterForm/>}/>
           <Route path= "/cart" element={<ShoppingCart/>} />
--- client/src/components/Target.js
+++ client/src/components/Target.js
@@ -4,46 +4,53 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import StarIcon from '@mui/icons-material/Star';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
 import "./css/Target.css"
 
-function Target() {
+function Target({ products }) {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          sx={{ height: 300 }}
-          component="img"
-          alt="green tomato"
-          image="https://www.agro.bayer.co/es-co/_jcr_content/root/responsivegrid/responsivegrid_copy_/promo_copy_copy_668018259.coreimg.jpeg/1661455349404/promo-tomate-agro-bayer-colombia.jpeg"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Producto
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Esta es la maravillosa descripcion que tendra el producto dependiendo de la base de datos
-          </Typography>
-          
-          <StarIcon style={{ color: 'yellow' }}/>
-          <StarIcon style={{ color: 'yellow' }}/>
-          <StarIcon style={{ color: 'yellow' }}/>
-          <StarIcon style={{ color: 'yellow' }}/>
-          <StarIcon style={{ color: 'yellow' }}/>
+    
+    <Card sx={{ maxWidth: 245 }}>
 
-        </CardContent>
-      </CardActionArea>
-      <CardActions style={
-        { display: 'flex', justifyContent: 'space-between' }
-      }>
-        <Button size="small" color="primary">
-          Añadir al carrito
-        </Button>
-        <Typography gutterBottom variant="h5" component="div">
-          $1000
-        </Typography>
-      </CardActions>
+      {products.map((product) => (
+        
+        
+      <>
+        <CardActionArea>
+          <CardMedia
+            sx={{ height: 200 }}
+            component="img"
+            alt= { product.nombre }
+            image= { product.imagen } />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              { product.nombre }
+            </Typography>
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <Typography variant="body2" color="text.secondary">
+                Unidades { product.stock } 
+              </Typography>
+
+              <Typography variant="body2" color="text.secondary">
+                { product.precio } $
+              </Typography>
+            </div>
+
+          </CardContent>
+        </CardActionArea>
+        <CardActions style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button size="small" color="primary">
+            Añadir al carrito
+          </Button>
+          <Button size="small" color="primary">
+            Info
+          </Button>  
+        </CardActions>
+      </>
+
+      ))}
     </Card>
+    
   );
 }
 
