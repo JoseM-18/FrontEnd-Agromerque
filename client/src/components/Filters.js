db18635b2397e0b8e4bdd9e00c7bef9e33e620fb
a-garcia07 <andres.m.munoz@correounivalle.edu.co>
2023-06-21 21:59:51 -0500

    Funcionalidad no visual de filtros

--- client/src/components/Filters.js
+++ client/src/components/Filters.js
@@ -1,9 +1,38 @@
-import Typography from '@mui/material/Typography';
+import React, { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { productContext } from './ProductContext';
+import { Container, Grid, Typography } from '@mui/material';
+import Target from './Target';
 import "./css/Filters.css"
 
 function Filters() {
+
+    const { productos } = useContext(productContext);
+    const { searchTerm } = useParams();
+
+    const [filters, setFilters] = useState({
+        minPrice: 0,
+        category: 'all'
+    });
+
+    const [filteredProducts, setFilteredProducts] = useState([]);
+
+    useEffect(() => {
+        const filtered = productos.filter(
+            product => product.salePrice >= filters.minPrice &&
+                (
+                    filters.category === 'all' ||
+                    product.category === product.category.includes(searchTerm)
+                )
+        );
+
+        setFilteredProducts(filtered);
+    }
+        , [searchTerm, productos]);
+
+
     return (
-        <section className="filters">
+        <><section className="filters">
 
             <div className="price">
                 <Typography variant='button' htmlFor="price">Precio</Typography>
@@ -11,21 +40,29 @@ function Filters() {
                     type="range"
                     id="price"
                     min="0"
-                    max="100"
-                />
+                    max="100" />
             </div>
 
             <div className="category">
                 <Typography variant='button' htmlFor="category">Categoria</Typography>
                 <select id="category">
-                    <option value="All">Todas</option>
+                    <option value="all">Todas</option>
                     <option value="Hortalizas">Hortalizas</option>
                     <option value="Frutas">Frutas</option>
                 </select>
             </div>
 
         </section>
+
+        <Container>
+            <Grid container spacing={4}>
+                <div className="body">
+                    <Target products={filteredProducts} />
+                </div>
+            </Grid>
+        </Container></>
     );
+
 }
 
 export default Filters
